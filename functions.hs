884986003c58e87b756b70c494ef197134218f0a
Alexander Hale <thalexander@openmailbox.org>
2016-12-17 12:27:51 +0000

    Fix cross product

--- functions.hs
+++ functions.hs
@@ -4,7 +4,7 @@ newtype Vector = Vector (Double,Double,Double)
 instance Num Vector where
     (Vector (a,b,c)) + (Vector (x,y,z)) = Vector (a+x,b+y,c+z) 
     (Vector (a,b,c)) - (Vector (x,y,z)) = Vector (a-x,b-y,c-z)
-    (Vector (a,b,c)) * (Vector (x,y,z)) = Vector (b*z - c*y, -(a*z - x*x), a*y - b*x)
+    (Vector (a,b,c)) * (Vector (x,y,z)) = Vector (b*z - c*y, -(a*z - c*x), a*y - b*x)
     negate (Vector (a,b,c)) = Vector (-a,-b,-c)
     abs (Vector (a,b,c)) = Vector (abs a, abs b, abs c)
     signum (Vector (a,b,c)) = Vector (signum a, signum b, signum c)
