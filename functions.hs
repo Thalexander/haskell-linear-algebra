8064c0b299ef20d1315b93ad2d3a67c35d58d78c
Alexander Hale <thalexander@openmailbox.org>
2016-12-17 11:55:28 +0000

    Implement Num type class for Vector type

--- functions.hs
+++ functions.hs
@@ -1,1 +1,12 @@
-type Vector = (Double,Double,Double)
+newtype Vector = Vector (Double,Double,Double)
+    deriving (Eq,Ord, Read, Show)
+    
+instance Num Vector where
+    (Vector (a,b,c)) + (Vector (x,y,z)) = Vector (a+x,b+y,c+z) 
+    (Vector (a,b,c)) - (Vector (x,y,z)) = Vector (a-x,b-y,c-z)
+    (Vector (a,b,c)) * (Vector (x,y,z)) = Vector (b*z - c*y, -(a*z - x*x), a*y - b*x)
+    negate (Vector (a,b,c)) = Vector (-a,-b,-c)
+    abs (Vector (a,b,c)) = Vector (abs a, abs b, abs c)
+    signum (Vector (a,b,c)) = Vector (signum a, signum b, signum c)
+    fromInteger a = Vector (fromInteger a, fromInteger a, fromInteger a)
+    
